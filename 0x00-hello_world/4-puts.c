7438bd1e82872b22fa36dc9b3c53ca9cca48740d
dammylove97 <wasyusuf@gmail.com>
2022-12-09 07:01:42 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
- *Main - Entry
+ *main - Entry
  *Return: Always 0
  */
-int main(void);
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
