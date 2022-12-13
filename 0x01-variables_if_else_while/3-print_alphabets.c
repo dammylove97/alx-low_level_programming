1a67636a198a9ac832aca9dace2dd85cae4069e6
dammylove97 <wasyusuf@gmail.com>
2022-12-13 03:17:19 -0800

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,9 +5,7 @@
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
-for (letter = 'a'; letter <= 'Z'; letter++)
+ for (letter = 'a'; letter <= 'z'; letter = 'A'; letter <= 'Z'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
