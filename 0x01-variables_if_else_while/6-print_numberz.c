56a67350d920bc55d508113006412d9b2136a924
dammylove97 <wasyusuf@gmail.com>
2022-12-14 04:51:09 -0800

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print numbers starting from 0 of base 10
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit 0;
+while (digit <= 9)
+{
+putchar(digit + '0');
+++digit;
+}
+putchar('\n');
+return (0);
+}
