61a5e6f6e2261946a543e5ffc6b974ec446b824b
dammylove97 <wasyusuf@gmail.com>
2022-12-14 05:46:12 -0800

    9 base

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - all possible combination of all digits 
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+++digit;
+}
+putchar('\n')
+return (0);
+}
