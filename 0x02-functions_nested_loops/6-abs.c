1d5c20f0f394df2c9b5b046b6d60d715ee80fd5d
dammylove97 <wasyusuf@gmail.com>
2022-12-25 15:23:54 -0800

    printing absolute integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  *_abs - prints the absolute integer value
+ *@n: print absolute interger 
  *Return: returns absolute 
  */
-int _abs(int)
+int _abs(int n)
 {
-abs(int);
-return (0);
+if (n < 0)
+n = (-1) * n;
+return (n);
 }
