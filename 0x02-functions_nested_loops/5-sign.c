bf679aaa1f57ddfc0f13f9c709b2d1ede451e003
dammylove97 <wasyusuf@gmail.com>
2022-12-25 14:58:30 -0800

    print sign on numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_sign - print the sign of a number
+ *@n: checks the function of the code
+ *Return: returns 1 if +, 0 if 0, -1 if -
+ */
+int print_sign(int n)
+{
+if (n > 0)
+_putchar(48 + 0);
+return (1);
+else if (n == 0)
+_putchar(0 + 48);
+return (0);
+else
+_putchar(45 + 0);
+return (-1);
+}
