d1eafc2e5637ed97c6e2ab4f97693d6ef4180567
dammylove97 <wasyusuf@gmail.com>
2022-12-25 08:06:20 -0800

    main files

---
+++ 0x02-functions_nested_loops/main/1-main.c
@@ -0,0 +1,11 @@
+#include "../main.h"
+/**
+*main - Entry points
+*Description: Check the code for Alx Students program
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+print_alphabet();
+return (0);
+}
