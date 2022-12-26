268e2e640c4886592a7a70d82b0f9dbb74bbb67f
dammylove97 <wasyusuf@gmail.com>
2022-12-25 16:23:06 -0800

    printing jack bauer busiest day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,11 +9,11 @@ for (hr = 0; hr <= 24; hr++)
 {
 for (min = 0; min <= 60; min++)
 {
-_putchar((hr / 10) + 48);
-_putchar((hr % 10) + 48);
+_putchar((hr / 1) + 48);
+_putchar((hr % 1) + 48);
 _putchar(':');
-_putchar((min / 10) + 48);
-_putchar((min / 10) + 48);
+_putchar((min / 1) + 48);
+_putchar((min / 1) + 48);
 _putchar('\n');
 }
 }
