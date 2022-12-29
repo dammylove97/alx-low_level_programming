443178f692d5cfbcd6b382098e5d014243aba195
dammylove97 <wasyusuf@gmail.com>
2022-12-29 03:52:00 -0800

    times table preparation

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*times_table - prints times table in multiple of 0 and 9
+*Example Table
+*0, 0, 0, 0
+*0, 1, 2, 3
+*/
+void times_table(void)
+int num, prod, mult;
+for (num = 0; num <= 9; ++num)
+{
+_putchar(48);
+for (mult = 1; mult <= 9, ++mult)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+/**
+*put space if the product is a single number
+*place the first digit if its two numbers
+*/
+if (prod <= 9)
+{
+_putchar(' ');
+else
+_putchar((prod / 10) + 48); /*getting the first digit*/
+_putchar((prod % 10) + 48); /*getting the second digit*/
+}
+_putchar('\n');
+}
+}
