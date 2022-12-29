dd4645832844185e63825876906af575207fbf05
dammylove97 <wasyusuf@gmail.com>
2022-12-29 06:26:43 -0800

    natural numbers printing"

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*Main - Entry points 
+*Description: prints all natural numbers multiple of 3 and 5
+*Return: Always 0 success
+*/
+int main(void)
+{
+int sum, num;
+for (num = 0; num < 1024; ++num)
+{
+if ((num % 3 == 0) || (num % 5 == 0))
+sum += num;
+}
+printf("%d\n", sum);
+return (0);
+}
