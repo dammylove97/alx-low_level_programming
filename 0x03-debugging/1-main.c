87d70ce69ab44fa732596c2c719adb9e37d92dec
dammylove97 <wasyusuf@gmail.com>
2022-12-30 07:35:19 -0800

    main debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - causes indefinite loop
+*Return: 0
+*/
+int main(void)
+{
+int i;
+printf("Infinete loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Indefinite loop avoided! \\o/\n");
+return (0);
+}
